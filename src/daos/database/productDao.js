25df56eb469169dda8ffd2e684b305c18c825def
Ivanproyectos <66085756+Ivanproyectos@users.noreply.github.com>
2024-09-03 12:00:05 -0500

    feat: listado de mis productos en el carro completado

--- src/daos/database/productDao.js
+++ src/daos/database/productDao.js
@@ -1,36 +1,27 @@
 import { Product } from '../../models/product.model.js'
 
 export default class ProductDao {
-    /**
-     * Retrieves all products from the database.
-     * @returns {Promise<Array<Product>>} Resolves with an array of all products.
-     */
+
     async getProducts () {
         const products = await Product.find().lean()
         return products
     }
 
-    /**
-     * Retrieves products from the database, paginated.
-     *
-     * @param {number} [page=1] The page of products to retrieve.
-     * @param {number} [limit=5] The number of products to retrieve per page.
-     * @param {string} [search=''] The search query to filter products by.
-     *
-     * @returns {Promise<object>} Resolves with an object containing the products, and pagination metadata.
-     */
-    async getProductsPaginated (page = 1, limit = 5, search = '') {
+    async getProductsPaginated (page = 1, limit = 5, search = '', order = 'asc') {
         const filter = {
             $or: [
                 { title: { $regex: search, $options: 'i' } },
                 { category: { $regex: search, $options: 'i' } },
-                { code: { $regex: search, $options: 'i' } }
+                { code: { $regex: search, $options: 'i' } },
+                //{ status: { $regex: search, $options: 'i' }}
             ]
         };
+        const sortPrice = order == 'asc' ? 1 : -1;
 
         const result = await Product.paginate(filter, {
             page,
             limit,
+            sort: { price: sortPrice },
             lean: true
         });
 
@@ -52,11 +43,6 @@ export default class ProductDao {
         await Product.findByIdAndDelete(id)
     }
 
-    /**
-     * Retrieves a product from the database by its code.
-     * @param {string} code The code of the product to search for.
-     * @returns {Promise<Product>} Resolves with the product if it exists, otherwise null.
-     */
     async getProductByCode(code){
         const product = await Product.findOne({code}).lean()
         return product
--- src/public/js/car.js
+++ src/public/js/car.js
@@ -1,8 +1,14 @@
 import { SweetOk, SweetError } from './utils/sweetAlert.js'
 
-const btnAddCar = document.querySelectorAll(`button[data-action="add-to-cart"]`)
+const addToCartButtons = document.querySelectorAll('button[data-action="add-to-cart"]')
+const showCarButton = document.querySelector('button[data-action="show-car"]')
+const hideCarButton = document.querySelector('button[data-action="hide-car"]')
+const modalshowCar = document.querySelector('.car')
+const carList = document.querySelector('.car__list')
 
-btnAddCar.forEach(btn => btn.addEventListener('click', handleClickAddToCart))
+addToCartButtons.forEach(btn => btn.addEventListener('click', handleClickAddToCart))
+showCarButton.addEventListener('click', () => { modalshowCar.classList.toggle('car--show') })
+hideCarButton.addEventListener('click', () => { modalshowCar.classList.toggle('car--show') })
 
 async function handleClickAddToCart() {
     const { product } = this.dataset
@@ -38,6 +44,8 @@ async function addProductToCar(carId, productId) {
         throw new Error('No se pudo agregar el producto');
     }
 
+    loadCarProducts(carId)
+
 }
 async function createCar() {
     const url = '/api/cars';
@@ -51,5 +59,52 @@ async function createCar() {
     return carId;
 }
 async function getProductsByCarId(carId){
+    const url = `/api/cars/${carId}`;
+    const response = await fetch(url);
 
+    if (!response.ok) {
+        throw new Error('error al obtener los productos del carrito', response.status);
+    }
+
+    return await response.json()
 }
+async function loadCarProducts(carId) {
+  const productsCountElement = document.querySelector(
+    '.header__cart-count'
+  );
+
+  try {
+    const { products } = await getProductsByCarId(carId)
+    const totalProducts = products.length
+
+    if(totalProducts > 0){
+        productsCountElement.textContent = products.length
+        productsCountElement.style.display = 'block'
+    }
+
+    carList.innerHTML = ''
+    carList.innerHTML = products.map(renderCarProduct).join('')
+  } catch (error) {
+    console.error(error)
+  }
+}
+function renderCarProduct({product, quantity}) {
+
+    const { title, price, image } = product;
+    return `<div class="car__item">
+    <div class="table-product__info">
+      <img src="/uploads/${image}" class="table-product__img" />
+      <div class="table-product__name">
+        <p>${title}</p>
+        <small class="table-product__code">$${price}</small>
+      </div>
+    </div>
+    <span>cant: ${quantity}</span>
+  </div>`
+}
+function init() {
+    const carId = localStorage.getItem('carId') ?? ''
+    loadCarProducts(carId)
+}
+
+init()
